8bf0d5bb98a70b098f840bf8ba5508104d59d97f
Hairston W <hairston39539@yahoo.com>
2019-01-11 22:31:55 -0500

    Add files via upload

---
+++ build/precache-manifest.42c9bdec94799b888f47b4ff4dd25fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9c3ad1b51d95f9c7921",
+    "url": "/static/js/main.d9c3ad1b.chunk.js"
+  },
+  {
+    "revision": "f2671ff7d32d5ea84930",
+    "url": "/static/js/1.f2671ff7.chunk.js"
+  },
+  {
+    "revision": "d9c3ad1b51d95f9c7921",
+    "url": "/static/css/main.87a07d22.chunk.css"
+  },
+  {
+    "revision": "f2671ff7d32d5ea84930",
+    "url": "/static/css/1.6fc8ec42.chunk.css"
+  },
+  {
+    "revision": "d5809044063afe2604a148a29b6eae42",
+    "url": "/index.html"
+  }
+];
